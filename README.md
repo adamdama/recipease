64cf123da9abaed722fc519719c45949c5b8580d
Adam Cox <adam.cox@hogarthww.com>
2020-06-02 12:03:39 +0100

    NestJS server app

--- README.md
+++ README.md
@@ -2,8 +2,19 @@
 
 ## Requirements
 
+### Client
+
 You need to have the vue cli installed to work on the front end. If you do not you can install it with the following:
 
 ```
 yarn global add @vue/cli
+```
+
+### Server
+
+You need to have the nestjs cli installed to work on the back end. If you do not you can install it with the following:
+
+```
+yarn global add @nestjs/cli
+yarn global add @nestjs/schematics
 ```
--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
-    extends: [
-        // TODO add TS
-        "prettier",
-        "eslint:recommended",
-        "plugin:prettier/recommended"
-    ],
-    plugins: [
-        "prettier"
-    ],
-    rules: {
-        "prettier/prettier": "error",
-        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-    }
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["prettier","@typescript-eslint/eslint-plugin"],
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "prettier/prettier": "error",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  },
 };
-
